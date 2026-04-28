16830cb621aee00e66b072070050bb993e2f880f
DESKTOP-RH4402K\kero <kirollosgerges090@gmail.com>
2026-04-28 17:39:39 +0300

    B==Fix Build issue

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,8 +18,10 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // Updated AGP to 8.9.1 to satisfy the androidx dependency requirements
+    id("com.android.application") version "8.9.1" apply false
+    // Updated Kotlin to 2.2.0 (Stable for April 2026)
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
 
 include(":app")
