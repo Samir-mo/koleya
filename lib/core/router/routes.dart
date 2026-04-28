6647470f612d892deade31a7c2e300b12b5ac371
DESKTOP-RH4402K\kero <kirollosgerges090@gmail.com>
2026-04-28 17:39:04 +0300

    Setup App Routers

---
+++ lib/core/router/routes.dart
@@ -0,0 +1,33 @@
+class Routes {
+  static const String HomeScreen = '/homeScreen';
+  static const String aboutScreen = '/aboutScreen';
+
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String welcome = '/welcome';
+  static const String login = '/login';
+  static const String signup = '/signup';
+
+  // Forget Password Flow
+  static const String forgetPassword = '/forgetPassword';
+  static const String getCode = '/getCode';
+  static const String resetPassword = '/resetPassword';
+
+  // Main App
+  static const String profile = '/profile';
+  static const String flights = '/flights';
+  static const String assistant = '/assistant';
+  static const String services = '/services';
+  static const String trackedFlight = '/trackedFlight';
+
+  // Services
+  static const String vipExperience = '/vipExperience';
+  static const String accessibility = '/accessibility';
+  static const String financial = '/financialServices';
+  static const String counters = '/flightCounters';
+  static const String exploreShops = '/exploreShops';
+  static const String serviceDetails = '/serviceDetails';
+
+  // Search
+  static const String search = '/search';
+}
