339d1088f80f1a06714b25373f193b12d16648cc
mustafaelbaz5 <m9stafa05@gmail.com>
2026-06-22 20:09:07 +0300

    MOve and update the custome app bar

--- lib/features/explore_places/ui/widgets/custom_app_bar.dart
+++ lib/features/explore_places/ui/widgets/custom_app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gate_buddy/core/themes/app_colors.dart';
+import 'package:gate_buddy/core/themes/app_text_styles.dart';
 
 class CustomAppBar extends StatelessWidget {
   final String appBarTitle;
@@ -15,28 +17,21 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      height: 80,
+      padding: const EdgeInsets.only(top: 32, left: 16, right: 16),
+      height: 100,
       width: double.infinity,
-      color: const Color(0xFF002D6B),
+      color: AppColors.primary200,
       child: Stack(
         alignment: Alignment.center,
         children: [
           if (leading != null)
             Positioned(
               left: 0,
-              child: GestureDetector(
-                onTap: onLeadingPressed,
-                child: leading,
-              ),
+              child: GestureDetector(onTap: onLeadingPressed, child: leading),
             ),
           Text(
             appBarTitle,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
+            style: AppTextStyles.font20Bold.copyWith(color: AppColors.white),
           ),
         ],
       ),
