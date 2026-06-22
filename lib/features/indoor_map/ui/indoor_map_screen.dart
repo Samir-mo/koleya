0bd578e793fa68f8f62f53dc9c4cb0e1d74c7dd6
mustafaelbaz5 <m9stafa05@gmail.com>
2026-06-22 19:26:04 +0300

    Remove the Unuesed Back Icon

--- lib/features/indoor_map/ui/indoor_map_screen.dart
+++ lib/features/indoor_map/ui/indoor_map_screen.dart
@@ -214,14 +214,6 @@ class _IndoorMapViewState extends State<_IndoorMapView> {
       color: _primaryBlue,
       child: Row(
         children: [
-          GestureDetector(
-            onTap: () => Navigator.pop(context),
-            child: const Icon(
-              Icons.arrow_back_ios_new,
-              color: Colors.white,
-              size: 20,
-            ),
-          ),
           const SizedBox(width: 12),
           Expanded(
             child: Column(
