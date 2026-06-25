3d503139cb14d77d5d0d2a82079ae8e385a94c75
mustafaelbaz5 <m9stafa05@gmail.com>
2026-06-25 07:30:45 +0300

    Create And Init Tracked Flight Feature

---
+++ lib/features/tracked_flight/data/remote/tracked_flight_remote_ds.dart
@@ -0,0 +1,13 @@
+﻿import 'package:gate_buddy/core/api/api_consumer.dart';
+import 'package:gate_buddy/core/api/api_endpoints.dart';
+
+class TrackedFlightRemoteDs {
+  final ApiConsumer api;
+  TrackedFlightRemoteDs({required this.api});
+
+  Future<dynamic> getTrackedFlights() => api.get(ApiEndpoints.trackedFlights);
+  Future<dynamic> trackFlight(String id) =>
+      api.post(ApiEndpoints.trackFlight.replaceFirst(':id', id));
+  Future<dynamic> untrackFlight(String id) =>
+      api.delete(ApiEndpoints.untrackFlight.replaceFirst(':id', id));
+}
---
+++ lib/features/tracked_flight/data/repo/tracked_flight_repo.dart
@@ -0,0 +1,5 @@
+﻿abstract class TrackedFlightRepo {
+  Future<dynamic> getTrackedFlights();
+  Future<dynamic> trackFlight(String id);
+  Future<dynamic> untrackFlight(String id);
+}
